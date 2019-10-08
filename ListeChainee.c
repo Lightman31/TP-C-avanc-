d0b58a8641803ddcc11431125f39835122d17263
Marine Beurotte <marine_beurotte@hotmail.fr>
2019-10-08 16:59:42 +0200

    Ajout de insertElem et lengthOfChain

--- ListeChainee.c
+++ ListeChainee.c
@@ -34,7 +34,24 @@ return start;
 
 }
 
-void affListFirstToLast(t_chain *firstElem)
+
+int lengthOfChain(t_chain *firstElem)
+{
+	int cpt = 1;
+	t_chain *nextElem = firstElem;
+	int fin = 0;
+	printf("%d\n", nextElem->val);
+	while (fin == 0)
+	{
+		nextElem = nextElem->n;
+		if (nextElem->n == NULL)
+			fin = 1;
+		cpt++;
+	}
+	return cpt;
+}
+
+void readListFirstToLast(t_chain *firstElem)
 {
 	t_chain *nextElem = firstElem;
 	int fin = 0;
@@ -49,7 +66,7 @@ void affListFirstToLast(t_chain *firstElem)
 
 }
 
-void affListLastToFirst(t_chain *firstElem)
+void readListLastToFirst(t_chain *firstElem)
 {
 	t_chain *nextElem = firstElem;
 	int fin = 0;
@@ -72,16 +89,59 @@ void affListLastToFirst(t_chain *firstElem)
 }
 
 
+t_chain *insertElm(int position, t_chain *elm2insert, t_chain *firstElem)
+{
+
+
+	if (position < 0) position = 0;
+	else if (position > lengthOfChain(firstElem)) position = lengthOfChain(firstElem);
+	else position = 0;
+
+	//printf("position = %d\n", position);
+
+	int i=0;
+
+	start=(t_chain*)malloc(sizeof(t_chain));
+	start->val=1;
+	start->n=NULL;
+	start->p=NULL;
+
+	last = start;
+	
+	for(i=2; i<sizeList+1; i++)
+	{
+		newElm=(t_chain*)malloc(sizeof(t_chain));
+		newElm->val=i;
+		newElm->n=NULL;
+		last->n = newElm;
+		newElm->p = last;
+		last = newElm;
+	}
+
+return start;
+
+
+
+	return firstElem;
+
+}
 int main(){
 
 	t_chain *firstElem = createChainList(10);
 
-	printf("first to last\n");
-	affListFirstToLast(firstElem);
 
-	printf("last to first\n");
-	affListLastToFirst(firstElem);
+	printf("taille de la chaine = %d\n", lengthOfChain(firstElem));
 
+	printf("first t0 last\n");
+	readListFirstToLast(firstElem);
+
+	printf("last t0 first\n");
+	readListLastToFirst(firstElem);
+
+	firstElem = insertElm(5, firstElem, firstElem);
+
+	printf("first t0 last\n");
+	readListFirstToLast(firstElem);
 
 
 }
